f4a61900c90a85123fab061197d5681261f479e8
Erwan ROUSSEL <erwan.roussel51@gmail.com>
2018-08-12 12:52:18 +0200

    Add Create and Select All request to the js lib

--- libs/js/stor.js
+++ libs/js/stor.js
@@ -1,9 +1,57 @@
-import { textChangeRangeIsUnchanged } from "typescript";
+const fetch = require("node-fetch");
 
 // Stor.js API wrapper
-export default class Stor {
+class Stor {
     constructor(link, token) {
         this.link = link
         this.token = token
-    } 
+    }
+    Table (name) {
+        return new Table(this.link, this.token, name)
+    }
 }
+
+class Table {
+    constructor(link, token, name) {
+        this.link = link
+        this.token = token
+        this.name = name
+    }
+    async Init (content) { 
+        return await fetch(`${this.link}/table/`, {
+            method: 'POST',
+            body: JSON.stringify({name: this.name, content: content}),
+            headers: {
+                'Authentification': this.token,
+                'Content-Type': 'application/json'
+            }
+        })
+    }
+    async SelectAll() {
+        return await fetch(`${this.link}/query/${this.name}/all`, {
+            method: 'GET',
+            headers: {
+                'Authentification': this.token
+            }
+        })
+    }
+}
+
+exports.Stor = Stor
+exports.Table = Table
+
+stor = new Stor("http://localhost:8080", "password")
+
+let users = stor.Table("dbb33")
+
+/* Create DB
+users.Init([{name: 'bite'},{name: 'marie'}])
+.then(res => res.text())
+.then(body => console.log(body))
+*/
+
+/* Select All
+users.SelectAll()
+.then(res => res.json())
+.then(body => console.log(JSON.parse(body.content)))
+*/
