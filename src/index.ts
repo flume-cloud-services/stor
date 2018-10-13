84ad6faa060782c10a62b2533d5bd0ee41098972
Erwan ROUSSEL <erwan.roussel51@gmail.com>
2018-10-13 15:42:16 +0200

    Add cors

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,32 @@
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
 
 import { QueryRoutes, TableRoutes } from "./routes";
 import { Connect } from "./models";
 
-import { PortNumber } from "./config";
+import { PortNumber, Cors } from "./config";
 
 Connect();
 
 const app: express.Application = express();
 
 app.use(bodyParser.json());
+
+if(Cors.enabled) {
+    app.use(cors());
+} else if (Cors.whitelist) {
+    const whitelist: string[] = Cors.whitelist || [];
+    const corsOptions = {
+        origin: (origin: string, callback: Function) => {
+            if (whitelist.indexOf(origin) !== -1) {
+                callback(null, true)
+            } else {
+                callback(new Error('Not allowed by CORS'))
+            }
+        }
+    }
+}
 
 app.use("/table/", TableRoutes);
 app.use("/query/", QueryRoutes);
