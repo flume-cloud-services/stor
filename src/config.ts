f2b27991de0f5ea590b8230bb612ff890d958dfb
Erwan ROUSSEL <erwan.roussel51@gmail.com>
2018-12-09 16:25:01 +0100

    Update configuration method

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const MongoUri: string = "mongodb://localhost:27017/icy";
-export const PortNumber: number = 8080;
-export const AuthToken: string = "password";
-export const Cors: { enabled: boolean, whitelist?: string[] } = { enabled: true };
+export const MongoUri: string = process.env.STOR_MONGO_URI || "mongodb://localhost:27017/icy";
+export const PortNumber: number = Number(process.env.STOR_PORT) || 8080;
+export const AuthToken: string = process.env.STOR_PASSWORD || "password";
+export const Cors: { enabled: boolean, whitelist?: string[] } = { enabled: Number(process.env.STOR_CORS_ENABLED) == 0 ? false : true, whitelist: process.env.STOR_CORS_WHITELIST ? process.env.STOR_CORS_WHITELIST.split(",") : [] };
